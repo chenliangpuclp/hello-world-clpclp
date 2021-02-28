246138ce60abea33f9f58d231b3c6963cc1fd438
chenliangpuclp <chenliangpuclp@163.com>
2021-02-28 14:47:35 +0800

    required by the github

--- function1.c
+++ function1.c
@@ -4,7 +4,10 @@ statement2
 statement3
 statement4
 statement5
+<<<<<<< HEAD
 statement6
 statement7
 =======
 >>>>>>> parent of d79291b (title：add 5 statements)
+=======
+>>>>>>> parent of 2e3cf81 (title:+2 statements, change 2 statements)
