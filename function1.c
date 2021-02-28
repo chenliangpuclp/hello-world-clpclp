087fd73295f9b00761fd1c6189e695e516f8c8cf
chenliangpuclp <chenliangpuclp@163.com>
2021-02-28 16:51:34 +0800

    revert +5 statements

--- function1.c
+++ function1.c
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 statement1
 statement2
 statement3
@@ -7,6 +8,8 @@ statement4
 statement5
 statement6
 statement7
+=======
+>>>>>>> parent of d79291b (title：add 5 statements)
 =======
 >>>>>>> parent of d79291b (title：add 5 statements)
 =======
